2a4d7feac3fba67fb05a93961303d3f8055cdbd1
jackqlean <jackqlean@gmail.com>
2024-10-18 16:20:08 -0300

    Program 7 - Accounts for example in account of bank.

--- src/application/Program7.java
+++ src/application/Program7.java
@@ -4,19 +4,19 @@ import entities.Accounts;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
 
-public class Program {
+public class Program7 {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1001, "Alex", 1000.0);
+		Accounts acc1 = new Accounts(1001, "Alex", 1000.0);
 		acc1.withdraw(200.0);
 		System.out.println(acc1.getBalance());
 		
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		Accounts acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
 		acc2.withdraw(200.0);
 		System.out.println(acc2.getBalance());
 		
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		Accounts acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
 		acc3.withdraw(200.0);
 		System.out.println(acc3.getBalance());
 	}
