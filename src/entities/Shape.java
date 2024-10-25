99d841f5ec153e432a538587613924d0a414237e
jackqlean <jackqlean@gmail.com>
2024-10-25 15:18:45 -0300

    program 10 Good solution - Exceptions.

--- src/entities/Shape.java
+++ src/entities/Shape.java
@@ -1,5 +1,25 @@
 package entities;
 
-public class Shape {
-    
+import entities.enums.Color;
+
+public abstract class Shape {
+
+	private Color color;
+	
+	public Shape() {
+	}
+	
+	public Shape(Color color) {
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	public abstract double area();
 }
